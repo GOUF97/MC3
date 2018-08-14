9de815404dc0721b925c7cd74bbf0730fd82da19
muhammad sutrisno <msutrisno86@gmail.com>
2018-08-14 16:57:10 +0700

    Initial Commit

---
+++ MC3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MC3
+//
+//  Created by muhammad sutrisno on 14/08/18.
+//  Copyright © 2018 OJONESU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
